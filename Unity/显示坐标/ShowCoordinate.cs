4fa8dbd7906322772010e39299db8bdecc3f1174
ghluzhiyuan <632007401@qq.com>
2017-06-04 15:55:41 +0800

    Unity show coordinate

---
+++ Unity/显示坐标/ShowCoordinate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class ShowCoordinate : MonoBehaviour {
+	
+	void OnGUI(){
+		float x = Input.mousePosition.x;
+		float y = Input.mousePosition.y;
+		GUI.TextArea(new Rect(20,30,150,50), "x : " + x + " y : " + y); 
+	}
+}
+
