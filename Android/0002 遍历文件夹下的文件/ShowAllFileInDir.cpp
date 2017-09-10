beb6bfbb2bc26c23f7ff8f2422e908711f86ea46
ghluzhiyuan <632007401@qq.com>
2017-09-10 21:55:09 +0800

    Android show all files in directory

---
+++ Android/0002 遍历文件夹下的文件/ShowAllFileInDir.cpp
@@ -0,0 +1,35 @@
+﻿#include <dirent.h> 
+
+
+// define for compile
+struct DIR {
+};
+
+/**
+ * 打印目录了下的所有文件
+ */
+void *ShowAllFileInDir(const char * szPath) 
+{
+    struct DIR *pDir = NULL;
+    pDir = opendir(szPath);
+    if (pDir == NULL)
+    {
+        printf("pDir is NULL");
+        return;
+    }
+
+    struct dirent *pstEnt;
+    while ((pstEnt = readdir(pDir)) != NULL)
+    {
+        printf("pstEnt d_type = %X", pstEnt->d_type);
+        printf("pstEnt filename = %s", pstEnt->d_name);
+    }
+}
+
+
+void main()
+{
+    ShowAllFileInDir("/data/local/tmp/");
+}
+
+
